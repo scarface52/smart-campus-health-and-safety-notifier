db3b42d90677d8429b252795555659bbc6a3dd77
Serkan Can Yıldırım <serkancanyildirim@hotmail.com>
2025-12-21 14:58:40 +0300

    refactor: Update Incident data class
    This commit refactors the `Incident` data class by:
    * Removing the `id`, `type`, and `user` fields.
    * Adding `class_id` and `owner_id`.
    * Changing the type of `report_time` from a commented-out `Date` to `Instant`.
    * Removing an unused `User` import.

--- app/src/main/java/com/theretros/smartcampus/data/dataclasses/Incident.kt
+++ app/src/main/java/com/theretros/smartcampus/data/dataclasses/Incident.kt
@@ -1,18 +1,16 @@
 package com.theretros.smartcampus.data.dataclasses
 
-import com.theretros.smartcampus.data.dataclasses.User
 import kotlinx.serialization.Serializable
-import java.util.Date
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
 @Serializable
-data class Incident(
-    val id: Int,
+data class Incident @OptIn(ExperimentalTime::class) constructor(
     val title: String,
     val description: String?,
-    @Serializable
-    //val report_time:Date,
+    val report_time: Instant,
     val location: String,
-    val type: String,
-    val user: User, // (Reporter)
+    val class_id: Int,
+    val owner_id: Int, // (Reporter)
     val status: String,
 )
