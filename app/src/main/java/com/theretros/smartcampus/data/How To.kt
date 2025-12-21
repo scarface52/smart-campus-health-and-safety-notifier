ddba9c9fd2fb449cf55c666240fa6b5f218d5024
Serkan Can Yıldırım <serkancanyildirim@hotmail.com>
2025-12-21 15:41:21 +0300

    feat: Implement Supabase user insertion, incident insertion and incident filtering functions
    This commit introduces functions to interact with the Supabase backend.
    
    It adds functionality to:
    - Insert new incidents into the `incidents` table.
    - Insert new users into the `users` table.
    - Retrieve a list of followed incidents for a specific user, class, and status.
    
    The Supabase client variable has also been renamed from `client` to `supabase` for clarity.

--- app/src/main/java/com/theretros/smartcampus/data/How To.kt
+++ app/src/main/java/com/theretros/smartcampus/data/How To.kt
@@ -1,16 +1,21 @@
 package com.theretros.smartcampus.data
 
+import com.theretros.smartcampus.data.dataclasses.Incident
+import com.theretros.smartcampus.data.dataclasses.IncidentSummary
 import com.theretros.smartcampus.data.dataclasses.User
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.from
+import io.github.jan.supabase.postgrest.query.Columns
 import io.github.jan.supabase.storage.Storage
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
 
-val client = createSupabaseClient(
+val supabase = createSupabaseClient(
     supabaseUrl = DATABASE_URL,
     supabaseKey = anon
 ) {
@@ -19,45 +24,101 @@ val client = createSupabaseClient(
     install(Storage)
 }
 
-// Insert User Function
-suspend fun InsertUser(user: User) {
-    try {
-        client.from("users")
-            .insert(user)
-
-        println("Data successfully inserted!")
-    } catch (e: Exception) {
-        println("Error inserting data: ${e.message}")
-        throw e
+@OptIn(ExperimentalTime::class)
+fun insertIncident(
+                   title: String,
+                   description: String,
+                   report_time: Instant,
+                   location: String,
+                   class_id: Int,
+                   owner_id: Int,
+                   status: String) {
+    val incident = Incident( title, description, report_time, location, class_id, owner_id, status)
+    CoroutineScope(Dispatchers.IO).launch {
+        try {
+            supabase.from("incidents").insert(incident)
+            println("Incident successfully inserted!")
+        } catch (e: Exception) {
+            println("Error inserting incident: ${e.message}")
+            throw e
+        }
     }
 }
 
-// how to use suspend functions
-fun insert(){
-    // suspend functions must be inside a coroutine scope
+fun getFollowedIncidents(userId: Int, classId: Int, status: String): MutableList<IncidentSummary> {
+
+    val followedIncidents = mutableListOf<Int>()
+    val incidents = mutableListOf<IncidentSummary>()
+
     CoroutineScope(Dispatchers.IO).launch {
 
+        val result1 = supabase.from("followed_incidents").select(
+            columns = Columns.list(
+                "incident_id"
+            )
+        ) {
+            filter {
+                eq("user_id", userId)
+            }
+        }.decodeList<Int>()
+        followedIncidents.addAll(result1)
+
+        val result2 = supabase.from("incidents").select(
+            columns = Columns.list(
+                "incident_id",
+                "title",
+                "description",
+                "report_time",
+                "class_id",
+                "status"
+            )
+        ) {
+            filter {
+                and {
+                    isIn("incident_id", followedIncidents)
+                }
+                and {
+                    eq("class_id", classId)
+                }
+                and {
+                    eq("status", status)
+
+                }
+            }
+        }.decodeList<IncidentSummary>()
+        incidents.addAll(result2)
+    }
+    return incidents
+}
+
+// how to use suspend functions
+fun insertUser(user_id: Int,
+               name: String,
+               last_name: String,
+               email: String,
+               password: String,
+               faculty: String,
+               role: String,
+               jurisdiction: String){
+    val user = User(user_id, name, last_name, email, password, faculty, role, jurisdiction)
+    // suspend functions must be inside a coroutine scope
+    CoroutineScope(Dispatchers.IO).launch {
         // Data insertion
-        val user = User(
-            35,
-            "yusuf",
-            "karaca",
-            "ykaraca@hotmail.com",
-            "abi57levelım",
-            "engineering",
-            "user",
-            ""
-        )
-        InsertUser(user)
+        try {
+            supabase.from("users").insert(user)
+            println("User successfully inserted!")
+        } catch (e: Exception) {
+            println("Error inserting user: ${e.message}")
+            throw e
+        }
     }
 }
 
 // get all users
-
-fun GetAllUsers() {
+fun getAllUsers() {
     val users = mutableListOf<User>()
     CoroutineScope(Dispatchers.IO).launch {
-        val result = client.from("users").select().decodeList<User>()
+        val result = supabase.from("users").select().decodeList<User>()
         users.addAll(result)
         for (user in users) {
             println(user)
