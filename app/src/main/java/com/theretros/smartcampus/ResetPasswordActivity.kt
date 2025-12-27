e2fa47759e5d67a01a936c8ef37a33c3caf7cefc
Serkan Can Yıldırım <serkancanyildirim@hotmail.com>
2025-12-27 23:15:13 +0300

    feat: Implement email submission on reset password screen

--- app/src/main/java/com/theretros/smartcampus/ResetPasswordActivity.kt
+++ app/src/main/java/com/theretros/smartcampus/ResetPasswordActivity.kt
@@ -1,12 +1,19 @@
 package com.theretros.smartcampus
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class ResetPasswordActivity : AppCompatActivity() {
+
+    private lateinit var emailField: TextInputEditText
+    private lateinit var sendCodeButton: MaterialButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,18 @@ class ResetPasswordActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        emailField = findViewById(R.id.emailField)
+        sendCodeButton = findViewById(R.id.sendCodeButton)
+        setupListeners()
+    }
+
+    fun setupListeners() {
+        sendCodeButton.setOnClickListener {
+            val email = emailField.text.toString()
+            val intent = Intent(this, EnterCodeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
